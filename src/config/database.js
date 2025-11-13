8d69744392ec26bfac1e92279bee1d1e1873ac83
Menga wanji <martialwanji@gmail.com>
2025-11-13 14:36:18 +0100

    Add ssl on database config

--- src/config/database.js
+++ src/config/database.js
@@ -9,7 +9,7 @@ const  pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT), 
-  SSL: { rejectUnauthorized: false}
+  ssl: { rejectUnauthorized: false}
 })
 
 export default pool;
